e04e6074d64d8b0e0b6c9934106b6e6f9fde89e3
Santsoh <santsoh.onlinetraining@gmail.com>
2021-10-20 10:05:02 +0530

    test

---
+++ Sep-Selenium/src/test/java/sep21_TestNG-Raju/Emp.java
@@ -0,0 +1,58 @@
+package sep21_TestNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+
+public class Emp {
+  @Test
+  public void empInfo() {
+	  System.out.println("Emp name is Shiva");
+  }
+  
+  @Test
+  public void empInfo2() {
+	  System.out.println("Emp name is Santosh");
+  }
+  
+   @Test
+  public void empInfo3() {
+	  System.out.println("Emp name is Santosh");
+  }
+  
+   @Test
+  public void empInfo4() {
+	  System.out.println("Emp name is Santosh");
+  }
+     @Test
+  public void empInfo5() {
+	  System.out.println("Emp name is Santosh");
+  }
+   @Test
+  public void empInfo6() {
+	  System.out.println("Emp name is Santosh");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("i am from before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("i am from after class");
+  }
+
+  @BeforeMethod
+  public void BM() {
+	  System.out.println("I am from Before method");
+  }
+  
+  @AfterMethod
+  public void AM() {
+	  System.out.println("i am from after method");
+  }
+
+  
+}
